01eb1bc0a23729767b1355664cb8c18fd72f64e7
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-17 16:20:05 +0300

    Converted to full component structure from php file.

--- components/Footer/footer.php
+++ components/Footer/footer.php
@@ -1,25 +1,31 @@
-<div class="cookie-container flex-display justify-between">
-    <p>
-        This is a sample cookie message. You can change it from the <a class="link blue-text">/components/Footer/footer.php</a>.
-    </p>
-    <button class="cookie-btn">Got it</button>
-</div>
-<footer class="flex-display justify-between align-center">
-    <div class="footer-copyright flex-display gap-5 light-text width-100 justify-between">
-        <ul class="no-list-style flex-display gap-10">
-            <li><a class="no-decoration link blue-text" href="/help-center/about-us">About Us</a></li>
-            <li><a class="no-decoration link blue-text" href="/help-center/contact-us">Contact Us</a></li>
-            <li><a class="no-decoration link blue-text" href="/help-center/terms-of-use">Terms of Use</a></li>
-            <li><a class="no-decoration link blue-text" href="/help-center/privacy-policy">Privacy Policy</a></li>
-        </ul>
-        <div class="flex-display gap-5">
-            <p>© 2023 Fraud
-            <div class="blue-text bold-text">PHP</div> All Rights Reserved.</p>
-        </div>
+<?php
+function Footer($props = [])
+{
+    return <<<HTML
+    <div class="cookie-container flex-display justify-between">
+        <p>
+            This is a sample cookie message. You can change it from the <a class="link blue-text">/components/Footer/footer.php</a>.
+        </p>
+        <button class="cookie-btn">Got it</button>
     </div>
-</footer>
-<script src="/src/js/cookie.js"></script>
-<script type="module" src="/src/js/main.js"></script>
-</body>
-
-</html>
+    <footer data-type="fraud-component" class="flex-display justify-between align-center">
+        <div class="footer-copyright flex-display gap-5 light-text width-100 justify-between">
+            <ul class="no-list-style flex-display gap-10">
+                <li><a class="no-decoration link blue-text" href="/help-center/about-us">About Us</a></li>
+                <li><a class="no-decoration link blue-text" href="/help-center/contact-us">Contact Us</a></li>
+                <li><a class="no-decoration link blue-text" href="/help-center/terms-of-use">Terms of Use</a></li>
+                <li><a class="no-decoration link blue-text" href="/help-center/privacy-policy">Privacy Policy</a></li>
+            </ul>
+            <div class="flex-display gap-5">
+                <p>© 2023 Fraud
+                <div class="blue-text bold-text">PHP</div> All Rights Reserved. <div class="blue-text bold-text">v2.1.1</div>
+                </p>
+            </div>
+        </div>
+    </footer>
+    <script src="/src/js/cookie.js"></script>
+    <script type="module" src="/src/js/main.js"></script>
+    </body>
+    </html>
+HTML;
+}
--- components/Navbar/navbar.php
+++ components/Navbar/navbar.php
@@ -1,32 +1,31 @@
 <?php
-require_once("{$_SERVER['DOCUMENT_ROOT']}/config/defaults.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/config/defaults.php");
+function Navbar($props = [])
+{
+    $PAGE_TITLE = $props["title"] ?? DEFAULT_TITLE;
+    $PAGE_DESCRIPTION = $props["description"] ?? DEFAULT_DESCRIPTION;
+    $PAGE_KEYWORDS = $props["keywords"] ?? DEFAULT_KEYWORDS;
+    $PAGE_AUTHOR = $props["author"] ?? DEFAULT_AUTHOR;
+    $PAGE_FAVICON = $props["favicon"] ?? DEFAULT_FAVICON;
 
-$PAGE_TITLE = isset($PAGE_TITLE) ? $PAGE_TITLE : $DEFAULT_PAGE_TITLE; // Use default value if not defined
-$PAGE_DESCRIPTION = isset($PAGE_DESCRIPTION) ? $PAGE_DESCRIPTION : $DEFAULT_PAGE_DESCRIPTION; // Use default value if not defined
-$PAGE_KEYWORDS = isset($PAGE_KEYWORDS) ? $PAGE_KEYWORDS : $DEFAULT_PAGE_KEYWORDS; // Use default value if not defined
-$PAGE_AUTHOR = isset($PAGE_AUTHOR) ? $PAGE_AUTHOR : $DEFAULT_PAGE_AUTHOR; // Use default value if not defined
-$PAGE_FAVICON = isset($PAGE_FAVICON) ? $PAGE_FAVICON : $DEFAULT_PAGE_FAVICON; // Use default value if not defined
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $PAGE_DESCRIPTION ?>">
-    <meta name="keywords" content="<?= $PAGE_KEYWORDS ?>">
-    <meta name="author" content="<?= $PAGE_AUTHOR ?>">
-    <link rel="stylesheet" href="/src/css/main.css">
-    <link rel="stylesheet" href="/src/css/utils.css">
-    <link rel="shortcut icon" href="<?= $PAGE_FAVICON ?>" type="image/x-icon">
-    <script src="/src/js/plugins/jquery.js"></script>
-    <title><?= $PAGE_TITLE ?></title>
-</head>
-
+    return <<<HTML
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="{$PAGE_DESCRIPTION}">
+        <meta name="keywords" content="{$PAGE_KEYWORDS}">
+        <meta name="author" content="{$PAGE_AUTHOR}">
+        <link rel="stylesheet" href="/src/css/main.css">
+        <link rel="stylesheet" href="/src/css/utils.css">
+        <link rel="shortcut icon" href="{$PAGE_FAVICON}" type="image/x-icon">
+        <script src="/src/js/plugins/jquery.js"></script>
+        <title>{$PAGE_TITLE}</title>
+    </head>
 <body>
-    <nav class="navbar flex-display justify-space-around align-center">
+    <nav data-type="fraud-component" class="navbar flex-display justify-space-around align-center">
         <div class="navbar-item flex-display gap-5 align-center">
             <img class="navbar-svg large-svg no-drag" src="/src/imgs/static/content/favicon.svg" alt="">
             <h2 class="navbar-logo flex-display gap-5">Fraud <div class="blue-text bold-text">PHP</div>
@@ -48,3 +47,5 @@ $PAGE_FAVICON = isset($PAGE_FAVICON) ? $PAGE_FAVICON : $DEFAULT_PAGE_FAVICON; //
             </ul>
         </div>
     </nav>
+HTML;
+}
