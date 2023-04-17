60b504e0b776d3f3d118a8629d28cec32353b539
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-17 16:33:22 +0300

    Added data-type="fraud-component"

--- components/HelpCenterItem/helpcenteritem.php
+++ components/HelpCenterItem/helpcenteritem.php
@@ -2,7 +2,7 @@
 function HelpCenterItem($props)
 {
     return <<<HTML
-    <section id={$props["id"]} class="page-content secondary-bg">
+    <section data-type="fraud-component" id={$props["id"]} class="page-content secondary-bg">
         <div class="content white-text">
             <h1 class="header-title">{$props["title"]}</h1>
             <p class="header-sub-text">
