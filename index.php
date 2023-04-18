66ef1e96f93060b68c52c3b29821e6cc0583080b
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-18 13:52:37 +0300

    Update index.php
    Title structure

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/components/Footer/Footer.php");
 
 <section id="home" class="page-content">
     <div class="content white-text light-text">
-        <h1 class="flex-display gap-5 header-title bold-text">Fraud <div class="blue-text">PHP</div>
+        <h1 class="header-title bold-text">Fraud<span class="blue-text">PHP</span>
         </h1>
         <p class="header-text text-bg text-bg-p-1">Designed to make php development faster, easier, and more enjoyable</p>
         <p class="header-sub-text m-t-10"> Start by editing the <code class="light-blue-text link text-bg text-bg-p-hlf">index.php</code> file in the root directory.</p>
